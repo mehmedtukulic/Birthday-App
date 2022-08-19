d7c34fb2863a8341259cc097f47e1b238fad132b
mehmedtukulic <mehmed.tukulic@hotmail.com>
2022-08-19 15:24:18 +0200

    Initial Commit

---
+++ BirthdayApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BirthdayApp
+//
+//  Created by Mehmed on 8/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
